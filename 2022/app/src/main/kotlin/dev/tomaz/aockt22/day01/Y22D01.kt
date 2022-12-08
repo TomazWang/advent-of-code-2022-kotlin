3c962c96568232d0d8334d278ab45ecae71cf578
Tomaz Wang <TomazWang.tw@gmail.com>
2022-12-08 16:12:02 +0800

    feat: refactor

--- 2022/app/src/main/kotlin/dev/tomaz/aockt22/day01/Y22D01.kt
+++ 2022/app/src/main/kotlin/dev/tomaz/aockt22/day01/Y22D01.kt
@@ -4,6 +4,31 @@ import dev.tomaz.aockt22.utils.readInput
 
 
 fun main() {
+
+    fun inputToCalList(input: List<String>): List<Int> {
+        val calList = input.fold(mutableListOf<Int>(0)) { acc: MutableList<Int>, curr: String ->
+
+            if (curr.isBlank()) {
+                // if curr is blank, add a new number
+                acc.add(0)
+            } else {
+                val num = curr.toInt()
+                acc[acc.lastIndex] = acc.last() + num
+            }
+            // if curr is not blank, append number
+            acc
+        }
+        return calList
+    }
+
+    fun part1(input: List<String>): Int = inputToCalList(input).maxOrNull() ?: 0
+
+    fun part2(input: List<String>): Int = inputToCalList(input)
+        .sortedDescending()
+        .take(3)
+        .sum()
+
+
     val sampleInput = readInput(dayNum = 1, fileName = "sample.txt")
     val sampleP1Result = part1(sampleInput)
     println("sample p1 ans: ${sampleP1Result}")
@@ -20,37 +45,4 @@ fun main() {
 }
 
 
-fun part1(input: List<String>): Int {
-    val calList = inputToCalList(input)
-    return calList.maxOrNull() ?: 0
-}
-
-fun part2(input: List<String>): Int {
-    val calList = inputToCalList(input)
-    val sortedCalList = calList.sortedDescending()
-//    println("sortedCalList = ${sortedCalList}")
-    val sliced = sortedCalList.slice(0..2)
-//    println("sliced = ${sliced}")
-    val sum = sliced.sum()
-//    println("sum = ${sum}")
-    return sum
-}
-
-
-
-private fun inputToCalList(input: List<String>): List<Int> {
-    val calList = input.fold(mutableListOf<Int>(0)) { acc: MutableList<Int>, curr: String ->
-
-        if (curr.isBlank()) {
-            // if curr is blank, add a new number
-            acc.add(0)
-        } else {
-            val num = curr.toInt()
-            acc[acc.lastIndex] = acc.last() + num
-        }
-        // if curr is not blank, append number
-        acc
-    }
-    return calList
-}
 
